bf71170f89907f571608cfb0b9e789958312301d
richardlee-kr <richardl22245@gmail.com>
2024-07-03 12:37:19 +0900

    [FEAT] interface functions Normailize, DoRecognition

--- Assets/01.Script/RecognitionAlgoritm/IRecognizer.cs
+++ Assets/01.Script/RecognitionAlgoritm/IRecognizer.cs
@@ -1,5 +1,10 @@
 
+using System.Collections.Generic;
+
 public interface IRecognizer
 {
+    public DollarPoint[] Normalize(DollarPoint[] points, int n);
 
+    public (string, float) DoRecognition(DollarPoint[] points, int n, 
+        List<RecognitionManager.GestureTemplate> gestureTemplates);
 }
