6957f064d80f17f82f76acb1b7c363a75d87278e
richardlee-kr <richardl22245@gmail.com>
2024-07-20 23:15:48 +0900

    [FEAT] name for recognizer

--- Assets/01.Script/RecognitionAlgoritm/IRecognizer.cs
+++ Assets/01.Script/RecognitionAlgoritm/IRecognizer.cs
@@ -1,8 +1,8 @@
-
 using System.Collections.Generic;
 
 public interface IRecognizer
 {
+    public string GetName();
     public DollarPoint[] Normalize(DollarPoint[] points, int n);
 
     public (string, float) DoRecognition(DollarPoint[] points, int n, 
--- Assets/01.Script/RecognitionAlgoritm/Recognizer.cs
+++ Assets/01.Script/RecognitionAlgoritm/Recognizer.cs
@@ -1,4 +1,9 @@
-
 public class Recognizer
 {
+    protected string name;
+
+    protected Recognizer(string name)
+    {
+        this.name = name;
+    }
 }
